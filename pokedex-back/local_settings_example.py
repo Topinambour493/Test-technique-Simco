364e74b7bb357e1db9957354fdf1dfadaacf0853
Topinambour493 <corentin@DEKSTOP-713705.localdomain>
2023-02-21 15:00:15 +0100

    securisation du projet

--- pokedex-back/local_settings_example.py
+++ pokedex-back/local_settings_example.py
@@ -1,5 +1,8 @@
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "THE_MOST_SECRET_KEY_ISlkjnsdfljnslinvesoimsreljnrtblkjndbknljnergkjldfvjhbeviunerfiergiu4587y2h3r089uwev9jn98hf98jv3kjn"
 
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
--- pokedex-back/settings.py
+++ pokedex-back/settings.py
@@ -9,23 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
-from pathlib import Path
-
 import django_env_overrides
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "3q@7xs0v_+d5ltnv-0(%$ug!#v1q2(@gqe@leb52k5$#+ya%-%"
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "/var/www/static"
@@ -82,17 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "pokedex-back.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
